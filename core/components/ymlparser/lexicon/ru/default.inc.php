5b27000e6467508b575371070b92f3c71d271729
Pavel Zarubin <pavel@orendat.ru>
2019-12-11 03:37:21 +0300

    Change lexicon

--- core/components/ymlparser/lexicon/ru/default.inc.php
+++ core/components/ymlparser/lexicon/ru/default.inc.php
@@ -2,33 +2,34 @@
 include_once 'setting.inc.php';
 
 $_lang['ymlparser'] = 'YMLParser';
-$_lang['ymlparser_menu_desc'] = 'Пример расширения для разработки.';
-$_lang['ymlparser_intro_msg'] = 'Вы можете выделять сразу несколько предметов при помощи Shift или Ctrl.';
+$_lang['ymlparser_menu_desc'] = 'YML парсер';
+$_lang['ymlparser_intro_msg'] = 'Вы можете выделять сразу несколько задач при помощи Shift или Ctrl.';
 
-$_lang['ymlparser_items'] = 'Предметы';
+$_lang['ymlparser_items'] = 'Задачи';
 $_lang['ymlparser_item_id'] = 'Id';
 $_lang['ymlparser_item_name'] = 'Название';
-$_lang['ymlparser_item_description'] = 'Описание';
+$_lang['ymlparser_item_link'] = 'Ссылка';
+$_lang['ymlparser_item_parse_date'] = 'Дата парсинга';
 $_lang['ymlparser_item_active'] = 'Активно';
 
-$_lang['ymlparser_item_create'] = 'Создать предмет';
-$_lang['ymlparser_item_update'] = 'Изменить Предмет';
-$_lang['ymlparser_item_enable'] = 'Включить Предмет';
-$_lang['ymlparser_items_enable'] = 'Включить Предметы';
-$_lang['ymlparser_item_disable'] = 'Отключить Предмет';
-$_lang['ymlparser_items_disable'] = 'Отключить Предметы';
-$_lang['ymlparser_item_remove'] = 'Удалить Предмет';
-$_lang['ymlparser_items_remove'] = 'Удалить Предметы';
-$_lang['ymlparser_item_remove_confirm'] = 'Вы уверены, что хотите удалить этот Предмет?';
-$_lang['ymlparser_items_remove_confirm'] = 'Вы уверены, что хотите удалить эти Предметы?';
+$_lang['ymlparser_item_create'] = 'Создать задачу';
+$_lang['ymlparser_item_update'] = 'Изменить задачу';
+$_lang['ymlparser_item_enable'] = 'Включить задачу';
+$_lang['ymlparser_items_enable'] = 'Включить задачи';
+$_lang['ymlparser_item_disable'] = 'Отключить задачу';
+$_lang['ymlparser_items_disable'] = 'Отключить задачи';
+$_lang['ymlparser_item_remove'] = 'Удалить задачу';
+$_lang['ymlparser_items_remove'] = 'Удалить задачи';
+$_lang['ymlparser_item_remove_confirm'] = 'Вы уверены, что хотите удалить эту задачу?';
+$_lang['ymlparser_items_remove_confirm'] = 'Вы уверены, что хотите удалить эти задачи?';
 $_lang['ymlparser_item_active'] = 'Включено';
 
-$_lang['ymlparser_item_err_name'] = 'Вы должны указать имя Предмета.';
-$_lang['ymlparser_item_err_ae'] = 'Предмет с таким именем уже существует.';
-$_lang['ymlparser_item_err_nf'] = 'Предмет не найден.';
-$_lang['ymlparser_item_err_ns'] = 'Предмет не указан.';
-$_lang['ymlparser_item_err_remove'] = 'Ошибка при удалении Предмета.';
-$_lang['ymlparser_item_err_save'] = 'Ошибка при сохранении Предмета.';
+$_lang['ymlparser_item_err_name'] = 'Вы должны указать имя задачи.';
+$_lang['ymlparser_item_err_ae'] = 'Задача с таким именем уже существует.';
+$_lang['ymlparser_item_err_nf'] = 'Задача не найдена.';
+$_lang['ymlparser_item_err_ns'] = 'Задача не указана.';
+$_lang['ymlparser_item_err_remove'] = 'Ошибка при удалении задачи.';
+$_lang['ymlparser_item_err_save'] = 'Ошибка при сохранении задачи.';
 
 $_lang['ymlparser_grid_search'] = 'Поиск';
 $_lang['ymlparser_grid_actions'] = 'Действия';
